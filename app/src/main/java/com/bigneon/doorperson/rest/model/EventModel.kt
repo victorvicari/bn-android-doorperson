1e273ea444734778d335138e7011acb1627126e0
Srđan Radović - [SR] <srdjan.radovic@stanga.net>
2019-03-26 12:44:10 +0100

    Scanning event page finished. Code optimization

--- app/src/main/java/com/bigneon/doorperson/rest/model/EventModel.kt
+++ app/src/main/java/com/bigneon/doorperson/rest/model/EventModel.kt
@@ -1,11 +1,17 @@
 package com.bigneon.doorperson.rest.model
 
+import com.google.gson.annotations.SerializedName
+
 /****************************************************
  * Copyright (c) 2016 - 2019.
  * All right reserved!
  * Created by SRKI-ST on 21.03.2019..
  ****************************************************/
 class EventModel {
-    internal var name : String? = null
-    internal var promo_image_url : String? = null
+    internal var id: String? = null
+
+    internal var name: String? = null
+
+    @SerializedName("promo_image_url")
+    internal var promoImageURL: String? = null
 }
--- app/src/main/java/com/bigneon/doorperson/rest/request/AuthRequest.kt
+++ app/src/main/java/com/bigneon/doorperson/rest/request/AuthRequest.kt
@@ -1,11 +1,13 @@
-package com.bigneon.doorperson.rest.model
+package com.bigneon.doorperson.rest.request
 
 /****************************************************
  * Copyright (c) 2016 - 2019.
  * All right reserved!
  * Created by SRKI-ST on 20.03.2019..
  ****************************************************/
-class AuthToken {
-    internal var access_token: String? = null
-    internal var refresh_token: String? = null
+class AuthRequest {
+    internal var email: String? = null
+
+    internal var password: String? = null
+
 }
