3ac29fe6ba7cee610315e47675b93f3d5115dba1
Srđan Radović - [SR] <srdjan.radovic@stanga.net>
2019-03-28 12:56:43 +0100

    Search guest list implemented

--- app/src/main/java/com/bigneon/doorperson/activity/GuestListActivity.kt
+++ app/src/main/java/com/bigneon/doorperson/activity/GuestListActivity.kt
@@ -9,24 +9,29 @@ import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
-import com.bigneon.doorperson.R
 import com.bigneon.doorperson.adapter.GuestListAdapter
 import com.bigneon.doorperson.adapter.OnItemClickListener
 import com.bigneon.doorperson.adapter.addOnItemClickListener
 import com.bigneon.doorperson.auth.AppAuth
 import com.bigneon.doorperson.rest.RestAPI
+import com.bigneon.doorperson.rest.model.GuestModel
 import com.bigneon.doorperson.rest.response.GuestsResponse
 import kotlinx.android.synthetic.main.activity_guest_list.*
-import kotlinx.android.synthetic.main.content_scanning_event.*
+import kotlinx.android.synthetic.main.content_guest_list.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 class GuestListActivity : AppCompatActivity() {
     private val TAG = GuestListActivity::class.java.simpleName
     private var eventId: String = ""
+    private var guestListView: RecyclerView? = null
+    private var guestList: ArrayList<GuestModel>? = null
 
     private fun getContext(): Context {
         return this
@@ -34,7 +39,7 @@ class GuestListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_list)
+        setContentView(com.bigneon.doorperson.R.layout.activity_guest_list)
 
         setSupportActionBar(guest_list_toolbar)
 
@@ -43,8 +48,37 @@ class GuestListActivity : AppCompatActivity() {
         //this line shows back button
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        search_guest.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                charSequence: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                val searchWords = charSequence.split(" ")
+                val finallyFilteredGuestList = ArrayList<GuestModel>()
+
+                for (word in searchWords) {
+                    val filteredGuestList = guestList?.filter {
+                        it.firstName?.toLowerCase()!!.contains(word.toLowerCase()) || it.lastName?.toLowerCase()!!.contains(
+                            word.toLowerCase()
+                        )
+                    } as ArrayList<GuestModel>
+                    filteredGuestList.forEach { if (it !in finallyFilteredGuestList) finallyFilteredGuestList.add(it) }
+                }
+                guestListView?.adapter = GuestListAdapter(finallyFilteredGuestList)
+            }
+        })
+
         guest_list_toolbar.navigationIcon!!.setColorFilter(
-            ContextCompat.getColor(getContext(), R.color.colorAccent),
+            ContextCompat.getColor(getContext(), com.bigneon.doorperson.R.color.colorAccent),
             PorterDuff.Mode.SRC_ATOP
         )
 
@@ -64,18 +98,19 @@ class GuestListActivity : AppCompatActivity() {
             override fun onResponse(call: Call<GuestsResponse>, response: Response<GuestsResponse>) {
                 if (response.code() == 401) { //Unauthorized
                     Snackbar
-                        .make(scanning_events_layout, "Unauthorized request!", Snackbar.LENGTH_LONG)
+                        .make(scanning_guest_layout, "Unauthorized request!", Snackbar.LENGTH_LONG)
                         .setDuration(5000).show()
                     Log.e(TAG, "MSG:" + response.message() + ", CODE: " + response.code())
                 } else {
-                    val guestListView: RecyclerView = findViewById(com.bigneon.doorperson.R.id.guest_list_view)
-                    val guestList = response.body()!!.data
+                    guestListView = findViewById(com.bigneon.doorperson.R.id.guest_list_view)
+                    guestList = response.body()!!.data
 
-                    guestListView.layoutManager = LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false)
+                    guestListView?.layoutManager =
+                        LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false)
 
-                    guestListView.adapter = GuestListAdapter(guestList!!)
+                    guestListView?.adapter = GuestListAdapter(guestList!!)
 
-                    guestListView.addOnItemClickListener(object : OnItemClickListener {
+                    guestListView?.addOnItemClickListener(object : OnItemClickListener {
                         override fun onItemClicked(position: Int, view: View) {
                             val intent = Intent(getContext(), GuestActivity::class.java)
                             intent.putExtra("eventId", eventId)
@@ -89,7 +124,7 @@ class GuestListActivity : AppCompatActivity() {
 
             override fun onFailure(call: Call<GuestsResponse>, t: Throwable) {
                 Snackbar
-                    .make(scanning_events_layout, "Authentication error!", Snackbar.LENGTH_LONG)
+                    .make(scanning_guest_layout, "Authentication error!", Snackbar.LENGTH_LONG)
                     .setAction(
                         "RETRY"
                     ) { startActivity(Intent(getContext(), LoginActivity::class.java)) }.setDuration(5000).show()
