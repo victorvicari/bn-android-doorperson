eed615fdc89fbdd1a44d3fc891721aac0614d92e
Srđan Radović - [SR] <srki@simple-task.com>
2019-06-21 12:26:47 +0200

    Sync events

--- app/src/main/java/com/bigneon/doorperson/activity/EventsActivity.kt
+++ app/src/main/java/com/bigneon/doorperson/activity/EventsActivity.kt
@@ -72,7 +72,7 @@ class EventsActivity : AppCompatActivity() {
                 val intent = Intent(getContext(), ScanningEventActivity::class.java)
                 if (eventForSync.isNullOrEmpty()) {
                     SharedPrefs.setProperty(AppConstants.EVENT_FOR_SYNC + eventId, eventId)
-//                    SyncController.synchronizeAllTables(true)
+                    SyncController.synchronizeAllTables(true)
                     intent.putExtra("showWaitingProgressBar", true)
                 } else {
                     intent.putExtra("showWaitingProgressBar", false)
